9bdfccbd4682794c1093df5b9b549c97d955a91d
DaniDelia253 <90367091+DaniDelia253@users.noreply.github.com>
2022-01-03 21:14:01 -0600

    function to get movie info from title

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
-fetch('http://www.omdbapi.com/?apikey=301ca359&t=beauty+and+the+beast&plot=full')
-    .then(function(response) {
-        console.log(response);
-        response.json().then(function (data) {
-            console.log(data);
-        })
-    });
+
+var getMovieInformation = function(movie) {
+    var apiUrl = 'http://www.omdbapi.com/?apikey=301ca359&t=' + movie + '&plot=full';
+    fetch(apiUrl)
+        .then(function(response) {
+            response.json().then(function (data) {
+                console.log(data);
+            })
+        });
+}
+
+
