76c7cb633d9e4ce68fcd5cbcaf26fe5eb4676f36
jester <jaimebuddy98@gmail.com>
2022-01-05 20:03:08 -0600

    reverting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,6 @@
 
 const imdbApiKey = "k_bxw4k76r"
 
-const getRandomMovie = async () => {
-  await fetch(`https://imdb-api.com/en/API/Top250Movies/${imdbApiKey}`).then(response=>{
-      if(response.ok){
-          response.json().then(data=>{
-              const index = Math.floor(Math.random()*data.items.length)
-              getMovieInformation(data.items[index].title)
-          })
-      }
-  })
-}
-
 var getMovieInformation = function(movie) {
     var apiUrl = 'http://www.omdbapi.com/?apikey=301ca359&t=' + movie + '&plot=full';
     fetch(apiUrl)
@@ -34,6 +23,7 @@ var getMovieInformation = function(movie) {
 }
 
 var displayMovieData = function (movieInfo) {
+//probably way more info here than we need, but I just wanted to create all these so we can use what we want!
 
     const parseRatingData = (ratingArray) => {
 
@@ -43,6 +33,9 @@ var displayMovieData = function (movieInfo) {
             metaCritic: "N/A",
         }
 
+        console.log(ratingArray)
+
+        // if (ratingArray.length > 1) {
             for (let ratingSource of ratingArray) {
                 switch (ratingSource.Source) {
                     case "Internet Movie Database":
@@ -57,6 +50,9 @@ var displayMovieData = function (movieInfo) {
                 }
             }
             return ratings
+        // } else {
+        //     return ratings
+        // }
     }
 
     let movieDetails = {
@@ -71,6 +67,29 @@ var displayMovieData = function (movieInfo) {
 
     createMovieCard(movieDetails)
 
+    var movieTitle = JSON.stringify(movieInfo.Title);
+    console.log(movieTitle);
+
+    var moviePlot = JSON.stringify(movieInfo.Plot);
+    console.log(moviePlot);
+
+    var moviePoster = JSON.stringify(movieInfo.Poster);
+    console.log(moviePoster);
+
+    var movieRatings = movieInfo.Ratings;
+    for (var i = 0; i < movieRatings.length; i++) {
+        var rating = movieRatings[i].Source + ' gives this movie ' + movieRatings[i].Value;
+        console.log(rating);
+    }
+
+    var movieRuntime = JSON.stringify(movieInfo.Runtime);
+    console.log(movieRuntime);
+
+    var movieYear = JSON.stringify(movieInfo.Year);
+    console.log(movieYear);
+
+    var movieActors = JSON.stringify(movieInfo.Actors);
+    console.log(movieActors);
 }
 
 /**
@@ -179,9 +198,7 @@ const createMovieCard = (movieDetails) => {
     $("#Search-Cards").append(column)
 }
 
-/**
- * Submit movie handler
- */
+
 $("#form").submit(function(event){
     event.preventDefault();
     const input = $($(this)[0][0]).val().trim()
@@ -189,6 +206,3 @@ $("#form").submit(function(event){
     getMovieInformation(input)
 })
 
-$("#popular").click(async ()=>{
-    await getRandomMovie()
-})
