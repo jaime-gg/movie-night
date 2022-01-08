69a4bb83b16019f95770c7acfb343baf5322c322
moonryc <70920813+moonryc@users.noreply.github.com>
2022-01-07 18:06:46 -0600

    fixed error where the website could not request over http

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,7 +51,7 @@ const getRandomMovie = () => {
  * @param movie
  */
 var getMovieInformation = function (movie) {
-    var apiUrl = 'http://www.omdbapi.com/?apikey=301ca359&t=' + movie + '&plot=full';
+    var apiUrl = 'https://www.omdbapi.com/?apikey=301ca359&t=' + movie + '&plot=full';
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
